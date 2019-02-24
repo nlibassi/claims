f7bd9c258f91d4dbd255c96dbb5404b12d9c225b
Nick <nlibassi@gmail.com>
2019-02-25 02:47:24 +0300

    email settings, models (insured) edits

--- submission/models.py
+++ submission/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from datetime import datetime
 # Create your models here.
 
 class Products(models.Model):
@@ -9,7 +10,6 @@ class Products(models.Model):
 
     def __str__(self):
         return self.title
-
 
     class Meta:
         db_table = "tutorial_products"
@@ -36,3 +36,69 @@ class Sales(models.Model):
         db_table = "tutorial_product_sales"
         verbose_name = "Sale"
         verbose_name_plural = "Sales"
+
+
+# copied from Flask app - modify for Django
+class Insured(models.Model):
+    auto_increment_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=64, unique=True)
+    email = models.EmailField(max_length=120)
+    password_hash = models.CharField(max_length=128)
+    last_seen = models.DateTimeField(default=datetime.utcnow)
+    first_name = models.CharField(max_length=64)
+    middle_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    gender = models.CharField(max_length=1, help_text="Enter 'M' or 'F'")
+    date_of_birth = models.DateField()
+    air_id = models.CharField(max_length=20)
+    mailing_street = models.CharField(max_length=64)
+    mailing_optional = models.CharField(max_length=64)
+    mailing_city = models.CharField(max_length=64)
+    mailing_state = models.CharField(max_length=15)
+    mailing_zip = models.CharField(max_length=10)
+    mailing_country = models.CharField(max_length=64)
+    residence_country = models.CharField(max_length=64)
+    foreign_currency_default = models.CharField(max_length=64)
+    other_coverage = models.CharField(max_length=1, help_text="Enter 'Y' or 'N'")
+    other_insurance_co = models.CharField(max_length=64)
+    other_plan_name = models.CharField(max_length=64)
+    other_plan_id = models.CharField(max_length=64)
+    medicare_part_a = models.CharField(max_length=1, help_text="Enter 'Y' or 'N'")
+    medicare_part_b = models.CharField(max_length=1, help_text="Enter 'Y' or 'N'")
+    medicare_id = models.CharField(max_length=64)
+    full_time_student = models.CharField(max_length=1, help_text="Enter 'Y' or 'N'")
+    has_dependent = models.CharField(max_length=1, help_text="Enter 'Y' or 'N'")
+    
+    # leftover from Flask?
+    #not a db field but high-level view of relationship 
+    #between insureds and claims - a 'virtual field'
+    #claims = db.relationship('Claim', backref='author', lazy='dynamic')
+    #dependents = db.relationship('Dependent', backref='employee', lazy='dynamic')
+
+    def __str__(self):
+        return self.username
+    
+
+    # modify for Django
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], 
+                algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return Insured.query.get(id)
--- submission/render.py
+++ submission/render.py
@@ -14,9 +14,24 @@ class Render:
         template = get_template(path)
         html = template.render(params)
         response = BytesIO()
+        file = open("my.file.pdf", "wb")
         # here response gets 'populated' with the output from pisa
-        pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), response)
+        pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), file)
+        file.close()
         if not pdf.err:
             return HttpResponse(response.getvalue(), content_type='application/pdf')
         else:
             return HttpResponse("Error Rendering PDF", status=400)
+
+
+        @staticmethod
+        def render_to_file(path: str, params: dict):
+            template = get_template(path)
+            html = template.render(params)
+            # why the random number in the file name?
+            file_name = "{0}-{1}.pdf".format(params['request'].user.first_name, randint(1, 1000000))
+            # absolute path of the directory where the program/file resides: os.path.abspath(os.path.dirname("__file__"))
+            file_path = os.path.join(os.path.abspath(os.path.dirname("__file__")), "store", file_name)
+            with open(file_path, 'wb') as pdf:
+                pisa.pisaDocument(BytesIO(html.encode("UTF-8")), pdf)
+            return [file_name, file_path]
